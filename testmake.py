f91c77bce26aae7f8c8cd051f5d6aa726e9e3205
Chinmoy Acharjee <chinmoyacharjee15@gmail.com>
2018-04-21 23:20:15 +0600

    updated text data

--- testmake.py
+++ testmake.py
@@ -17,5 +17,5 @@ write_to_file('data10000.txt', data10000)
 data100000 = ''.join(random.choices(string.ascii_uppercase + string.digits, k=100000-100))+pattern
 write_to_file('data100000.txt', data100000)
                    
-data1000000 = ''.join(random.choices(string.ascii_uppercase + string.digits, k=100000-100))+pattern
+data1000000 = ''.join(random.choices(string.ascii_uppercase + string.digits, k=1000000-100))+pattern
 write_to_file('data1000000.txt', data1000000)
